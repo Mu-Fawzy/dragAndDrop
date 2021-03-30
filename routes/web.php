3f811bc3aa6b3cfbecb4875905b9c44a42c89cad
Mu-Fawzy <mrrmohamedfawzy@gmail.com>
2021-03-30 14:24:58 +0200

    22 commit

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Backend\HomeController@index');
+Route::get('/home', 'Backend\HomeController@index');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function() {
