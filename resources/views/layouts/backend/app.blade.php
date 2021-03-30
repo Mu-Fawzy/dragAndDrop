532e00d8fa19e9f87d795dd27913a684d2521975
Mu-Fawzy <mrrmohamedfawzy@gmail.com>
2021-03-30 15:05:06 +0200

    24 commit

--- resources/views/layouts/backend/app.blade.php
+++ resources/views/layouts/backend/app.blade.php
@@ -40,13 +40,13 @@
                         @guest('admin')
                             @if (Route::has('admin.login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin.login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('admin.login') }}">{{ __('تسجيل الدخول') }}</a>
                                 </li>
                             @endif
                             
                             @if (Route::has('admin.register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin.register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('admin.register') }}">{{ __('تسجيل جديد') }}</a>
                                 </li>
                             @endif
                         @else
@@ -59,7 +59,7 @@
                                     <a class="dropdown-item" href="{{ route('admin.logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('الخروج') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" class="d-none">
